b20974ecae587c2315a4f868c3dfb75b3d18f7b4
rohanasurvase <rohanassurvase@gmail.com>
2021-07-14 16:41:47 +0530

    Created a basic component structure

---
+++ src/Components/HeaderComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function HeaderComponent(){
+	return(
+		<div>
+		ABC
+		</div>
+	);
+}
+export default HeaderComponent;
---
+++ src/Components/MainComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function MainComponent(props){
+
+		return(
+
+			<div>
+				<div>{props.name.abc}</div>
+				<div>B</div>
+				<div>C</div>
+			</div>
+		);
+	}
+
+
+export default MainComponent;
